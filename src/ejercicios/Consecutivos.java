ff3090bddb4e1e54690d5ee5834289d14f817939
Usuario DAM 1 <Usuario DAM 1@DESKTOP-R6MG806>
2019-10-15 11:45:25 +0200

    Merge origin/master

--- src/ejercicios/Consecutivos.java
+++ src/ejercicios/Consecutivos.java
@@ -1,9 +1,25 @@
 package ejercicios;
-
+import java.util.Scanner;
 /**
  *
  * @author Diego_Eloy DAM 1
  */
 public class Consecutivos {
-    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Dime 3 numeros:");
+        int numero1 = teclado.nextInt();
+        int numero2 = teclado.nextInt();
+        int numero3 = teclado.nextInt();
+        
+        if(numero1<numero2 && numero1<numero3){
+            System.out.printf("El %d > %d > %d",numero1,numero2,numero3);
+        }else if(numero2<numero3 && numero2>numero1 && numero1<numero3){
+            System.out.printf("El %d > %d > %d",numero2,numero1,numero3);
+        }else if(numero3<numero2 && numero3<numero1 && numero1<numero2){
+            System.out.printf("El %d > %d > %d",numero3,numero1,numero2);
+        }
+        
+        
+    }
 }
